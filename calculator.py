ab6da9e28de687d23c1cea4af666ec1679f87269
AittalebAyoub <ayo.aittaleb@gmail.com>
2026-09-14 11:48:22 +0100

    add linting formating problem

--- calculator.py
+++ calculator.py
@@ -1,12 +1,20 @@
-def add(a, b):
-    return a + b
+import os
+
+
+def add(a,b):
+    return a+b
+
 
 def subtract(a, b):
     return a - b
+
 
 def divide(a, b):
     if b == 0:
         raise ValueError("Division par zéro impossible")
     return a / b
 
-# comment
+
+def calculate_square(x):
+    unused_variable = 99
+    return x * x
